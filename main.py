1f9bbee05a465a81477619383a25ae26ad03d62b
PTW-105 <jstemp105@gmail.com>
2015-07-26 21:51:36 -0600

    Implementing non-functional Steam directory discovery via registry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # IMPORTS
 import os
+import winreg
 import hashlib
 
 
@@ -9,6 +10,22 @@ g_variable = ""
 
 # FUNCTIONS
 def main():
-    pass
+
+    find_steam_dir()
+
+def execute():
+    os.system('')
+
+
+def find_steam_dir():
+    l_steam_directory = ""
+    l_steam_dir_found = False
+    l_possible_steam_dirs = ['C:\\Program Files\\Steam\\steamapps\\common\\']
+
+#    l_steam_reg_path = r"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 107410"
+    l_steam_reg_path = r'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    l_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r'SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 107410', 0, (winreg.KEY_WOW64_64KEY + winreg.KEY_ALL_ACCESS))
+    val = winreg.QueryValue(l_key, "InstallLocation")
+    return(val)
 
 main()
