92d5bbb6e7ddbb5edce0d48487c87cc703ceaa71
Schutzwolf <ewlarson93@gmail.com>
2015-07-26 23:05:40 -0600

    Return Fixed

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from subprocess import Popen
 
 # GLOBAL VARIABLES
 g_variable = ""
-g_param= []
-g_npm = 0
-move = True
 
 # FUNCTIONS
 def main():
@@ -53,35 +50,38 @@ def reg_find_steam_dir():
     return(val)
 
 def parameters():
+    l_param= []
+    l_npm = 0
+    l_move = True
     while move is True:
-            print("Number of Parameters Entered", g_npm)
-            i = input("Enter a Parameter: ")
-            g_param.append(i)
-            call = input("Add Another Parameter?(Y,N) ")
-            call = str.lower(call)
-            if call == "y":
-                print("Continuing Parameter Input")
-                g_npm += 1
-            elif call == "n":
-                move = False
-                print("Exiting Parameter Input...")
-                print("Parameters Selected:")
-            else:
-                fault = False
-                while fault is False:
-                    call2 = input("Invalid Input, Please Try Again: ")
-                    call2 = str.lower(call2)
-                    if call2 == "y":
-                        print("Continuing Parameter Input")
-                        move = True
-                        g_npm += 1
-                        fault = True
-                    elif call2 == "n":
-                        move = False
-                        fault = True
-                        print("Exiting Parameter Input...")
-                        print("Parameters Selected:")
-                    else:
-                        fault = False
-                        pass
+        print("Number of Parameters Entered", l_npm)
+        i = input("Enter a Parameter: ")
+        l_param.append(i)
+        call = input("Add Another Parameter?(Y,N) ")
+        call = str.lower(call)
+        if call == "y":
+            print("Continuing Parameter Input")
+            l_npm += 1
+        elif call == "n":
+            move = False
+            print("Exiting Parameter Input...")
+            print("Parameters Selected:")
+        else:
+            fault = False
+            while fault is False:
+                call2 = input("Invalid Input, Please Try Again: ")
+                call2 = str.lower(call2)
+                if call2 == "y":
+                    print("Continuing Parameter Input")
+                    move = True
+                    l_npm += 1
+                    break
+                elif call2 == "n":
+                    move = False
+                    print("Exiting Parameter Input...")
+                    print("Parameters Selected:")
+                    break
+                else:
+                    fault = False
+    return(l_param)
 main()
